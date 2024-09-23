a52bb2aee026d347a96d6c6aa5fd2350bdb5e672
Yunuuuu <yunyunpp96@outlook.com>
2024-09-23 20:41:08 +0800

    Enable the usage of ordering integer index directly in `align_reorder`

--- R/align-reorder.R
+++ R/align-reorder.R
@@ -1,8 +1,10 @@
 #' Reorders layout observations based on weights or summary statistics.
 #'
-#' @param fun A summary function. It should take a data and return the
-#' statistic, which we'll call [order2()] to extract the order information.
-#' @param ... Additional arguments passed to `fun`.
+#' @param order A summary function. It should take a data and return the
+#' statistic, which we'll call [order2()] to extract the order information. You
+#' can also provide the ordering integer index and wrap it with [I()].
+#' @param ... Additional arguments passed to function provided in `order`
+#' argument.
 #' @param strict A boolean value indicates whether the order should be strict.
 #' If previous groups has been established, and strict is `FALSE`, this will
 #' reorder the observations in each group.
@@ -15,17 +17,23 @@
 #'     align_reorder()
 #' @seealso [order2()]
 #' @export
-align_reorder <- function(fun = rowMeans, ..., strict = TRUE,
+align_reorder <- function(order = rowMeans, ..., strict = TRUE,
                           reverse = FALSE, data = NULL,
                           set_context = FALSE, name = NULL) {
-    fun <- rlang::as_function(fun)
+    if (!inherits(order, "AsIs")) {
+        order <- rlang::as_function(order)
+    } else if (!is.numeric(order) || anyNA(order) || anyDuplicated(order)) {
+        cli::cli_abort(
+            "{.arg order} must be a numeric without missing value or ties"
+        )
+    }
     assert_bool(strict)
     assert_bool(reverse)
     align(
         align_class = AlignReorder,
         params = list(
-            fun = fun,
-            fun_params = rlang::list2(...),
+            order = order,
+            order_params = rlang::list2(...),
             reverse = reverse,
             strict = strict
         ),
@@ -38,20 +46,33 @@ align_reorder <- function(fun = rowMeans, ..., strict = TRUE,
 
 #' @importFrom ggplot2 ggproto
 AlignReorder <- ggproto("AlignReorder", Align,
-    compute = function(self, panel, index, fun, fun_params, strict) {
+    compute = function(self, panel, index, order, order_params, strict) {
         data <- .subset2(self, "data")
         assert_reorder(self, panel, strict)
-        ans <- rlang::inject(fun(data, !!!fun_params))
-        ans <- order2(ans)
-        assert_mismatch_nobs(self,
-            nrow(data), length(ans),
-            msg = "must return a statistic with",
-            arg = "fun"
+        if (inherits(order, "AsIs")) {
+            ans <- as.integer(order)
+            if (any(ans < 1L) || any(ans > nrow(data))) {
+                cli::cli_abort(
+                    "Outliers found in the provided ordering index",
+                    call = .subset2(self, "call")
+                )
+            }
+            msg <- "must be an ordering integer index of"
+            index <- ans
+        } else {
+            ans <- rlang::inject(order(data, !!!order_params))
+            index <- as.integer(order2(ans))
+            msg <- "must return a statistic with"
+        }
+        assert_mismatch_nobs(
+            self, nrow(data), length(ans),
+            msg = smg, arg = "order"
         )
+        self$index <- index
         ans
     },
     layout = function(self, panel, index, reverse) {
-        index <- .subset2(self, "statistics")
+        index <- .subset2(self, "index")
         if (reverse) index <- rev(index)
         list(panel, index)
     }
