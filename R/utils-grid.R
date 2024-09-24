e3a4bf372ea8322806204a5e3fd8d66297b81427
Yunuuuu <yunyunpp96@outlook.com>
2024-09-24 18:07:20 +0800

    remove unused functions

--- R/utils-grid.R
+++ R/utils-grid.R
@@ -13,21 +13,3 @@ is_absolute_unit <- function(x) {
 
 #' @importFrom grid unitType
 is_null_unit <- function(x) unitType(x) == "null"
-
-get_width <- function(x) UseMethod("get_width")
-get_height <- function(x) UseMethod("get_height")
-
-#' @importFrom grid grobWidth
-#' @export
-get_width.default <- function(x) grobWidth(x)
-
-#' @importFrom grid grobHeight
-#' @export
-get_height.default <- function(x) grobHeight(x)
-
-#' @importFrom gtable gtable_width
-get_width.gtable <- function(x) gtable_width(x)
-
-#' @importFrom gtable gtable_height
-#' @export
-get_height.gtable <- function(x) gtable_height(x)
