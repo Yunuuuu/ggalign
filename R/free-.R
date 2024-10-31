aa0fe5544e27e1c7462c2778da081fd6cbd877be
Yunuuuu <yunyunpp96@outlook.com>
2024-11-01 02:13:46 +0800

    collect all functions of ggfree into single file

--- R/free-.R
+++ R/free-.R
@@ -82,6 +82,21 @@ ggplot_add.free_gg <- function(object, plot, object_name) {
     object
 }
 
+free_add <- function(object, free, object_name) UseMethod("free_add")
+
+#' @importFrom ggplot2 ggplot_add
+#' @export
+free_add.default <- function(object, free, object_name) {
+    free$plot <- ggplot_add(object, .subset2(free, "plot"), object_name)
+    free
+}
+
+#' @export
+free_add.plot_action <- function(object, free, object_name) {
+    free$action <- update_action(free$action, object)
+    free
+}
+
 free_build <- function(x, action, direction) {
     plot <- plot_add_action(.subset2(x, "plot"), action, call = quote(ggfree()))
     list(plot = plot, size = .subset2(x, "size"))
