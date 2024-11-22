5d3e6098cbd7cded5266e6f69cbc65699c289d82
Yunuuuu <yunyunpp96@outlook.com>
2024-11-22 16:55:35 +0800

    use magick to capture the image directly

--- R/raster-magick.R
+++ R/raster-magick.R
@@ -16,16 +16,7 @@
 #' image.
 #'
 #' @param ... Not used currently.
-#' @param dpi An integer Sets the desired resolution in dots per inch, if NULL,
-#' the function estimates the DPI based on the current device.
-#' @param dev A string of `r oxford_or(c("png", "jpeg", "tiff"))` indicates The
-#' graphical output format for rasterization. If the rendering backend is set to
-#' `r oxford_or(c("agg", "cairo"))`, by default, no explicit device will be
-#' used, otherwise, the default device used is "png".
-#' @param backend A string of `r oxford_or(c("agg", "cairo"))`. If the parameter
-#' is set to `NULL`, the function will using the `grDevices` package for
-#' rendering. Defaults to either `agg` or `cairo` (whichever is available). If
-#' neither is available, falls back to `grDevices`.
+#' @param res An integer sets the desired resolution in pixels.
 #' @inheritParams grid::rasterGrob
 #' @examples
 #' # data generated code was copied from `ComplexHeatmap`
@@ -48,37 +39,15 @@
 #' @return An object with the same class of the input.
 #' @export
 raster_magick <- function(x, magick = NULL, ...,
-                          dpi = NULL, dev = NULL,
-                          backend = waiver(), interpolate = FALSE) {
+                          res = NULL, interpolate = FALSE) {
     rlang::check_installed("magick", "to use `raster_magick()`")
     if (!is.null(magick) && !is.function(magick <- allow_lambda(magick))) {
         cli_abort("{.arg magick} must be a function")
     }
-    assert_number_whole(dpi, min = 1, allow_null = TRUE)
-    if (is.waive(backend)) {
-        if (rlang::is_installed("ragg")) {
-            backend <- "agg"
-        } else if (rlang::is_installed("Cairo")) {
-            backend <- "cairo"
-        } else {
-            backend <- NULL
-        }
-    } else if (!is.null(backend)) {
-        backend <- match.arg(backend, c("cairo", "agg"))
-        if (backend == "cairo") {
-            rlang::check_installed("Cairo", "to use `backend = 'cairo'`")
-        } else {
-            rlang::check_installed("ragg", "to use `backend = 'agg'`")
-        }
-    }
-    if (is.null(backend) || !is.null(dev)) {
-        dev <- match.arg(dev, c("png", "jpeg", "tiff"))
-    }
+    assert_number_whole(res, min = 1, allow_null = TRUE)
     .raster_magick(
         x = x, ...,
-        magick = magick, dpi = dpi,
-        dev = dev, backend = backend,
-        interpolate = interpolate
+        magick = magick, interpolate = interpolate
     )
 }
 
@@ -126,17 +95,12 @@ raster_magick <- function(x, magick = NULL, ...,
 
 #' @export
 .raster_magick.grob <- function(x, magick = NULL, ...,
-                                dpi = NULL, dev = NULL,
-                                backend = waiver(),
-                                interpolate = FALSE) {
+                                res = NULL, interpolate = FALSE) {
     if (inherits(x, "zeroGrob")) {
         return(x)
     }
     x$ggalign_raster_magick <- list(
-        magick = magick,
-        dpi = dpi,
-        dev = dev,
-        backend = backend,
+        magick = magick, res = res,
         interpolate = interpolate
     )
     add_class(x, "ggalign_raster_magick")
@@ -164,11 +128,9 @@ makeContext.ggalign_raster_magick <- function(x) {
     # Grab grob metadata
     vp <- .subset2(x, "vp") %||% viewport()
     params <- .subset2(x, "ggalign_raster_magick")
-    dpi <- .subset2(params, "dpi") %||%
+    res <- .subset2(params, "res") %||%
         # If missing, take current DPI
         convertWidth(unit(1, "inch"), "pt", valueOnly = TRUE)
-    dev <- .subset2(params, "dev")
-    backend <- .subset2(params, "backend")
     magick <- .subset2(params, "magick")
     interpolate <- .subset2(params, "interpolate")
 
@@ -184,73 +146,20 @@ makeContext.ggalign_raster_magick <- function(x) {
     on.exit(grDevices::dev.set(old_dev), add = TRUE)
 
     # Render layer
-    if (is.null(dev)) { # No device specified
-        # Setup temporary device for capture
-        if (backend == "cairo") {
-            dev_id <- Cairo::Cairo(
-                type = "raster", width = width, height = height,
-                units = "in", dpi = dpi, bg = NA
-            )
-            grid::pushViewport(vp)
-            grid::grid.draw(x)
-            grid::popViewport()
-            raster <- grid::grid.cap()
-            grDevices::dev.off(which = dev_id)
-        } else {
-            ragg_cap <- ragg::agg_capture(
-                width = width, height = height,
-                units = "in", res = dpi,
-                background = NA
-            )
-            grid::pushViewport(vp)
-            grid::grid.draw(x)
-            grid::popViewport()
-            raster <- ragg_cap(native = TRUE)
-            grDevices::dev.off()
-        }
-        if (!is.null(magick)) {
-            image <- magick(magick::image_read(raster))
-            on.exit(magick::image_destroy(image), add = TRUE)
-            raster <- grDevices::as.raster(image)
-        }
-    } else {
-        file <- tempfile(fileext = sprintf(".%s", dev))
-        # Temporarily make a file to write png to
-        # Destroy temporary file upon function exit
-        on.exit(file.remove(file), add = TRUE)
-        if (is.null(backend)) {
-            dev <- getFromNamespace(dev, "grDevices")
-            dev_id <- dev(
-                file,
-                width = width, height = height,
-                units = "in", res = dpi, bg = NA,
-                type = "cairo"
-            )
-        } else if (backend == "cairo") {
-            dev_id <- Cairo::Cairo(
-                type = dev,
-                width = width, height = height,
-                units = "in", dpi = dpi, bg = NA
-            )
-        } else {
-            dev <- getFromNamespace(sprintf("agg_%s", dev), "ragg")
-            dev_id <- dev(
-                file,
-                width = width, height = height,
-                units = "in", res = dpi, background = NA
-            )
-        }
-        grid::pushViewport(vp)
-        grid::grid.draw(x)
-        grid::popViewport()
-        grDevices::dev.off(which = dev_id)
-        image <- magick::image_read(file)
-        if (!is.null(magick)) {
-            image <- magick(image)
-            on.exit(magick::image_destroy(image), add = TRUE)
-        }
-        raster <- grDevices::as.raster(image)
+    image <- magick::image_graph(
+        width = width, height = height,
+        bg = NA, res = res,
+        antialias = FALSE
+    )
+    grid::pushViewport(vp)
+    grid::grid.draw(x)
+    grid::popViewport()
+    grDevices::dev.off()
+    if (!is.null(magick)) {
+        image <- magick(image)
+        on.exit(magick::image_destroy(image), add = TRUE)
     }
+    raster <- grDevices::as.raster(image)
 
     # Forward raster grob
     grid::rasterGrob(
