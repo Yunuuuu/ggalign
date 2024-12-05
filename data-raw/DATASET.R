14a909a27a65bc3a42371369897ddf977dbeedac
Yunuuuu <yunyunpp96@outlook.com>
2024-12-05 23:43:07 +0800

    update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -27,18 +27,8 @@ base_mean <- rowMeans(mat)
 mat_scaled <- t(apply(mat, 1, scale))
 type <- gsub("s\\d+_", "", colnames(mat))
 
-logo <- ggstack(data = mat_scaled) +
-    stack_active(sizes = c(0.2, 1, 1)) +
-    # group stack rows into 5 groups
-    align_kmeans(centers = 5L) +
-    # add a block plot for each group in the stack
-    ggalign(size = unit(1, "cm"), data = NULL) +
-    geom_tile(aes(x = 1, fill = factor(.panel))) +
-    scale_fill_brewer(palette = "Dark2", name = "Kmeans group") +
-    scale_x_continuous(breaks = NULL, name = NULL) +
-    # add a heatmap plot in the stack
-    ggheatmap() +
-    quad_active(free_spaces = "l") +
+heat1 <- ggheatmap(mat_scaled) -
+    plot_align(free_spaces = "l") +
     scale_y_continuous(breaks = NULL) +
     scale_fill_viridis_c(option = "magma") +
     # add dendrogram for this heatmap
@@ -48,9 +38,8 @@ logo <- ggstack(data = mat_scaled) +
     ggalign(data = type, size = unit(1, "cm")) +
     geom_tile(aes(y = 1, fill = factor(value))) +
     scale_y_continuous(breaks = NULL, name = NULL) +
-    scale_fill_brewer(palette = "Set1", name = "type") +
-    # add another heatmap in the stack and set the heatmap body width
-    ggheatmap(base_mean, .width = unit(2, "cm")) +
+    scale_fill_brewer(palette = "Set1", name = "type")
+heat2 <- ggheatmap(base_mean, width = unit(2, "cm")) +
     scale_y_continuous(breaks = NULL) +
     scale_x_continuous(name = "base mean", breaks = FALSE) +
     scale_fill_gradientn(colours = c("#2600D1FF", "white", "#EE3F3FFF")) +
@@ -62,23 +51,14 @@ logo <- ggstack(data = mat_scaled) +
     geom_boxplot(aes(y = value, fill = factor(.extra_panel))) +
     scale_x_continuous(expand = expansion(), breaks = NULL) +
     scale_fill_brewer(palette = "Dark2", guide = "none") +
-    theme(axis.title.y = element_blank()) +
-    # we move into the stack layout
-    stack_active() +
-    # add a point plot
-    ggalign(data = expr$length, size = unit(2, "cm")) +
-    geom_point(aes(x = value)) +
-    labs(x = "length") +
-    theme(
-        panel.border = element_rect(fill = NA),
-        axis.text.x = element_text(angle = -60, hjust = 0)
-    ) +
-    # add another heatmap and set the heatmap body width
-    ggheatmap(expr$type, width = unit(2, "cm")) +
+    theme(axis.title.y = element_blank())
+
+heat3 <- ggheatmap(expr$type, width = unit(2, "cm")) +
     scale_fill_brewer(palette = "Set3", name = "gene type") +
     scale_x_continuous(breaks = NULL, name = "gene type") +
     # add barplot in the top annotation, and remove the spaces in the y-axis
-    anno_top(free_spaces = "lr") +
+    anno_top() -
+    plot_align(free_spaces = "lr") +
     ggalign(limits = FALSE) +
     geom_bar(
         aes(.extra_panel, fill = factor(value)),
@@ -92,6 +72,31 @@ logo <- ggstack(data = mat_scaled) +
         plot.background = element_blank(), panel.background = element_blank(),
         legend.background = element_blank()
     )
+logo <- stack_alignh(data = mat_scaled) +
+    stack_active(sizes = c(0.2, 1, 1)) +
+    # group stack rows into 5 groups
+    align_kmeans(centers = 5L) +
+    # add a block plot for each group in the stack
+    ggalign(size = unit(1, "cm"), data = NULL) +
+    geom_tile(aes(x = 1, fill = factor(.panel))) +
+    scale_fill_brewer(palette = "Dark2", name = "Kmeans group") +
+    scale_x_continuous(breaks = NULL, name = NULL) +
+    # add a heatmap plot in the stack
+    heat1 +
+    # add another heatmap in the stack and set the heatmap body width
+    heat2 +
+    # we move into the stack layout
+    stack_active() +
+    # add a point plot
+    ggalign(data = expr$length, size = unit(2, "cm")) +
+    geom_point(aes(x = value)) +
+    labs(x = "length") +
+    theme(
+        panel.border = element_rect(fill = NA),
+        axis.text.x = element_text(angle = -60, hjust = 0)
+    ) +
+    # add another heatmap and set the heatmap body width
+    heat3
 logo_tmp <- tempfile(fileext = ".png")
 ggplot2::ggsave(logo_tmp, plot = logo, device = "png", dpi = 1000L)
 hexSticker::sticker(
