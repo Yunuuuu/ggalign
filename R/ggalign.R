b7a9f47c17e23b1c7ffa4ed1282e9825e2eddd93
Yunuuuu <yunyunpp96@outlook.com>
2025-01-01 11:41:45 +0800

    fix error for S4 matrix

--- R/ggalign.R
+++ R/ggalign.R
@@ -147,7 +147,7 @@ AlignGg <- ggproto("AlignGg", AlignProto,
                 }
                 design["nobs"] <- list(layout_nobs)
                 # we always add `.index` to align the observations
-                if (is.matrix(input_data)) {
+                if (identical(length(dim(input_data)), 2L)) {
                     plot_data$.index <- vec_rep(
                         seq_len(NROW(input_data)),
                         NCOL(input_data)
