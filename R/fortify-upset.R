e101591a6f6b8fc48e5f0036bfa02506be9bb5b1
Yunuuuu <yunyunpp96@outlook.com>
2025-01-06 01:26:17 +0800

    add support for UpSet plot

---
+++ R/fortify-upset.R
@@ -0,0 +1,140 @@
+#' Build a Matrix for UpSet plot
+#'
+#' @description
+#' `r lifecycle::badge('experimental')`
+#'
+#' This function converts various objects into a matrix format suitable for
+#' creating an UpSet plot. It always returns a matrix for a `horizontal` UpSet
+#' plot.
+#'
+#' @param data An object to be converted to a matrix for UpSet  plot.
+#' @param mode A string of `r oxford_or(c("distinct", "intersect", "union"))`
+#' indicates the mode to define the set intersections. Check
+#' <https://jokergoo.github.io/ComplexHeatmap-reference/book/upset-plot.html#upset-mode>
+#' for details.
+#' @inheritParams rlang::args_dots_used
+#' @return A matrix.
+#' @seealso
+#' - [`fortify_upset.list()`]
+#' - [`fortify_upset.matrix()`]
+#' @export
+fortify_upset <- function(data, mode = "distinct", ...) {
+    mode <- arg_match0(mode, c("distinct", "intersect", "union"))
+    rlang::check_dots_used()
+    UseMethod("fortify_upset")
+}
+
+#' @inherit fortify_upset
+#' @param data A list of sets.
+#' @param ... Not used.
+#' @section ggalign attributes:
+#'  - `intersection_sizes`: An integer vector indicating the size of each
+#'    intersection.
+#'  - `set_sizes`: An integer vector indicating the size of each set.
+#' @family fortify_upset methods
+#' @export
+fortify_upset.list <- function(data, mode = "distinct", ...) {
+    data <- lapply(data, function(x) {
+        vec_unique(vec_slice(x, !vec_detect_missing(x)))
+    })
+    data <- list_drop_empty(data)
+
+    # Based on the explanation from
+    # https://jokergoo.github.io/ComplexHeatmap-reference/book/upset-plot.html
+    action <- switch(mode,
+        distinct = function(data, intersection) {
+            out <- NULL
+            for (i in which(intersection)) {
+                if (is.null(out)) {
+                    out <- .subset2(data, i)
+                } else if (vec_size(out) == 0L) { # early exit for empty items
+                    return(out)
+                } else {
+                    out <- vec_set_intersect(out, .subset2(data, i))
+                }
+            }
+            for (i in which(!intersection)) {
+                if (vec_size(out) == 0L) { # early exit for empty items
+                    return(out)
+                }
+                out <- vec_set_difference(out, .subset2(data, i))
+            }
+            return(out)
+        },
+        intersect = function(data, intersection) {
+            out <- NULL
+            for (i in which(intersection)) {
+                if (is.null(out)) {
+                    out <- .subset2(data, i)
+                } else if (vec_size(out) == 0L) { # early exit for empty items
+                    return(out)
+                } else {
+                    out <- vec_set_intersect(out, .subset2(data, i))
+                }
+            }
+            out
+        },
+        union = function(data, intersection) {
+            Reduce(vec_set_union, .subset(data, intersection))
+        }
+    )
+
+    intersection <- logical(vec_size(data)) # template
+    intersection_and_size <- lapply(
+        seq_len(vec_size(intersection)),
+        function(n) {
+            apply(
+                # generate all possible intersections
+                utils::combn(vec_size(intersection), n), 2L,
+                function(index) {
+                    intersection[index] <- TRUE
+                    list(
+                        intersection = intersection,
+                        # for each intersection, we define the size
+                        size = vec_size(action(data, intersection))
+                    )
+                },
+                simplify = FALSE
+            )
+        }
+    )
+
+    # https://en.wikipedia.org/wiki/UpSet_plot
+    # UpSets can be used horizontally and vertically.
+    # In a vertical UpSet plot, the columns of the matrix correspond to the
+    # sets, the rows correspond to the intersections.
+    # we by default use `horizontal` upset, the rows of the matrix correspond
+    # to the sets, the columns correspond to the intersections.
+    ans <- list_transpose(unlist(intersection_and_size, FALSE, FALSE))
+    intersections <- inject(cbind(!!!.subset2(ans, "intersection")))
+    rownames(intersections) <- names(data)
+    intersection_sizes <- unlist(.subset2(ans, "size"), FALSE, FALSE)
+    keep <- intersection_sizes > 0L # remove intersection without items
+    intersections <- intersections[, keep, drop = FALSE]
+    intersection_sizes <- intersection_sizes[keep]
+    ggalign_attr_set(intersections, list(
+        intersection_sizes = intersection_sizes,
+        set_sizes = list_sizes(data),
+        upset_mode = mode
+    ))
+}
+
+#' @inherit fortify_upset.list
+#' @param data A matrix where each row represents an element, and each column
+#' defines a set. The values in the matrix indicate whether the element is
+#' part of the set. Any non-missing value signifies that the element exists in
+#' the set.
+#' @family fortify_upset methods
+#' @export
+fortify_upset.matrix <- function(data, mode = "distinct", ...) {
+    data <- !is.na(data)
+    elements <- vec_seq_along(data)
+    fortify_upset.list(
+        apply(data, 2L,
+            function(index) .subset(elements, index),
+            simplify = FALSE
+        ),
+        mode = mode,
+        ...
+    )
+}
---
+++ R/layout-quad-upset.R
@@ -0,0 +1,202 @@
+#' Draw UpSet plot
+#'
+#' @description
+#' `r lifecycle::badge('experimental')`
+#'
+#' `ggupset` is a specialized version of [`quad_discrete()`], which simplifies
+#' the creation of Upset plot.
+#'
+#' @param data Data used to create Upset plot. [`fortify_upset()`] will be used
+#' to convert data to a matrix.
+#' @param ... Additional arguments passed to [`fortify_upset()`].
+#' @inheritParams quad_discrete
+#' @param direction A string indicating the direction of the UpSet plot,
+#' `"h"`(`horizontal`) or `"v"`(`vertical`). In a vertical UpSet plot, the
+#' columns of the matrix correspond to the sets, and the rows correspond to the
+#' intersections. By default, the horizontal UpSet plot is used, where the rows
+#' of the matrix correspond to the sets and the columns correspond to the
+#' intersections.
+#' @param point A list of parameters passed to
+#' [`geom_point()`][ggplot2::geom_point()].
+#' @param line A list of parameters passed to
+#' [`geom_line()`][ggplot2::geom_line()].
+#' @param rect A list of parameters passed to
+#' [`geom_rect()`][ggplot2::geom_rect()].
+#' @inheritSection quad_discrete ggplot2 specification
+#' @examples
+#' set.seed(123)
+#' lt <- list(
+#'     a = sample(letters, 5),
+#'     b = sample(letters, 10),
+#'     c = sample(letters, 15)
+#' )
+#' ggupset(lt) +
+#'     scale_fill_manual(values = c("#F0F0F0", "white"), guide = "none") +
+#'     scale_color_manual(values = c("grey", "black"), guide = "none") +
+#'     anno_top() +
+#'     ggalign(data = function(d) ggalign_attr(d, "intersection_sizes")) +
+#'     ggplot2::geom_bar(aes(y = .data$value), stat = "identity") +
+#'     anno_right() +
+#'     ggalign(data = function(d) ggalign_attr(d, "set_sizes")) +
+#'     ggplot2::geom_bar(aes(x = .data$value),
+#'         stat = "identity",
+#'         orientation = "y"
+#'     )
+#' @importFrom ggplot2 aes
+#' @export
+ggupset <- function(data = NULL, mapping = aes(),
+                    ...,
+                    direction = "h",
+                    point = NULL, line = NULL, rect = NULL,
+                    width = NA, height = NA,
+                    theme = NULL, active = NULL) {
+    UseMethod("ggupset")
+}
+
+# Don't allow inherit from the parent layout, since data for upset plot is
+# usually different with others
+#' @export
+ggupset.NULL <- function(data, ...) {
+    cli_abort("{.arg data} must be provided to create upset plot")
+}
+
+#' @export
+ggupset.waiver <- ggupset.NULL
+
+#' @importFrom ggplot2 aes
+#' @export
+ggupset.default <- function(data = NULL, mapping = aes(),
+                            ...,
+                            direction = "h",
+                            point = NULL, line = NULL, rect = NULL,
+                            width = NA, height = NA,
+                            theme = NULL, active = NULL) {
+    direction <- check_direction(direction)
+    # we need a matrix to melted into long formated data frame
+    data <- fortify_upset(data = data, ...)
+    if (is_vertical(direction)) data <- ggalign_attr_restore(t(data), data)
+    ans <- new_quad_layout(
+        name = "ggupset",
+        data = data,
+        mapping = mapping,
+        theme = theme,
+        active = active
+    )
+    ans@plot <- ggadd_default(ans@plot, mapping = aes(.data$.x, .data$.y)) +
+        ggplot2::labs(x = NULL, y = NULL) +
+        upset_rect(direction, rect) +
+        upset_point(point) +
+        upset_line(direction, line)
+    ans
+}
+
+merge_mapping <- function(x, y) {
+    if (is.null(x)) {
+        return(y)
+    }
+    for (i in names(y)) {
+        x[[i]] <- .subset2(y, i)
+    }
+    x
+}
+
+merge_data_fn <- function(plot_data, user_data) {
+    if (is.null(user_data) || is.waive(user_data)) {
+        plot_data
+    } else if (is.function(user_data <- allow_lambda(user_data))) {
+        force(plot_data)
+        function(data) user_data(plot_data(data))
+    } else {
+        user_data
+    }
+}
+
+upset_rect <- function(direction, rect) {
+    if (is_horizontal(direction)) {
+        rect$mapping <- merge_mapping(rect$mapping, aes(
+            ymin = .data$.ymin, ymax = .data$.ymax, fill = .data$rect_group
+        ))
+        rect$data <- merge_data_fn(function(data) {
+            column <- c(
+                ".y", ".panel_x", ".panel_y",
+                ".row_index", ".row_names", ".discrete_y"
+            )
+            o <- vec_unique(data[vec_set_intersect(column, names(data))])
+            o$rect_group <- (.subset2(o, ".y") %% 2L) == 0L
+            o$.ymin <- o$.y - 0.5
+            o$.ymax <- o$.y + 0.5
+            o
+        }, rect$data)
+        inject(ggplot2::geom_rect(!!!rect,
+            xmin = -Inf, xmax = Inf, inherit.aes = FALSE
+        ))
+    } else {
+        rect$mapping <- merge_mapping(rect$mapping, aes(
+            xmin = .data$.xmin, xmax = .data$.xmax,
+            fill = .data$rect_group
+        ))
+        rect$data <- merge_data_fn(function(data) {
+            column <- c(
+                ".x", ".panel_x", ".panel_y",
+                ".column_index", ".column_names", ".discrete_x"
+            )
+            o <- vec_unique(data[vec_set_intersect(column, names(data))])
+            o$rect_group <- (.subset2(o, ".x") %% 2L) == 0L
+            o$.xmin <- o$.x - 0.5
+            o$.xmax <- o$.x + 0.5
+            o
+        }, rect$data)
+        inject(ggplot2::geom_rect(!!!rect,
+            ymin = -Inf, ymax = Inf, inherit.aes = FALSE
+        ))
+    }
+}
+
+upset_point <- function(point) {
+    point$mapping <- merge_mapping(point$mapping, aes(
+        x = .data$.x, y = .data$.y, color = .data$point_group
+    ))
+    point$data <- merge_data_fn(function(data) {
+        data$point_group <- .subset2(data, "value")
+        data
+    }, point$data)
+    inject(ggplot2::geom_point(!!!point, inherit.aes = FALSE))
+}
+
+upset_line <- function(direction, line) {
+    line$mapping <- merge_mapping(line$mapping, switch_direction(
+        direction,
+        aes(.data$.x, .data$.y, group = paste(.data$.panel_x, .data$.x)),
+        aes(.data$.x, .data$.y, group = paste(.data$.panel_y, .data$.y))
+    ))
+    line$data <- merge_data_fn(function(data) {
+        if (is_horizontal(direction)) {
+            dlist <- vec_split(data, data[c(".panel_x", ".x")])
+            dlist <- lapply(.subset2(dlist, "val"), function(d) {
+                o <- vec_slice(d, .subset2(d, "value"))
+                if (vec_size(o) < 2L) {
+                    return(NULL)
+                }
+                vec_slice(o, c(
+                    which.min(.subset2(o, ".y")),
+                    which.max(.subset2(o, ".y"))
+                ))
+            })
+            vec_rbind(!!!dlist)
+        } else {
+            dlist <- vec_split(data, data[c(".panel_y", ".y")])
+            dlist <- lapply(.subset2(dlist, "val"), function(d) {
+                o <- vec_slice(d, .subset2(d, "value"))
+                if (vec_size(o) < 2L) {
+                    return(NULL)
+                }
+                vec_slice(o, c(
+                    which.min(.subset2(o, ".x")),
+                    which.max(.subset2(o, ".x"))
+                ))
+            })
+            vec_rbind(!!!dlist)
+        }
+    }, line$data)
+    inject(ggplot2::geom_line(!!!line, inherit.aes = FALSE))
+}
