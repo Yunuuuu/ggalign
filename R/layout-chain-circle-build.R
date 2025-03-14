a1054b016cc4c9c5b7470118049c3552557ce1ab
Yunuuuu <yunyunpp96@outlook.com>
2025-03-14 12:59:34 +0800

    respect the free_facet

--- R/layout-chain-circle-build.R
+++ R/layout-chain-circle-build.R
@@ -112,15 +112,18 @@ circle_build <- function(circle, schemes = NULL, theme = NULL) {
             ) * 0.5,
             layout_name = align$layout_name
         )
-        if (!align$free_facet && is_discrete_design(design) &&
-            nlevels(.subset2(design, "panel")) > 1L) {
-            plot <- plot + facet_sector(
-                ggplot2::vars(.data$.panel), plot_coord,
-                spacing_theta = circle@spacing_theta %||% pi / 180,
-                drop = FALSE
-            )
+        if (!align$free_facet && is_discrete_design(design)) {
+            if (nlevels(.subset2(design, "panel")) > 1L) {
+                plot <- plot + facet_sector(
+                    ggplot2::vars(.data$.panel), plot_coord,
+                    spacing_theta = circle@spacing_theta %||% pi / 180,
+                    drop = FALSE
+                )
+            } else {
+                plot <- gguse_facet(plot, ggplot2::facet_null())
+                plot$coordinates <- plot_coord
+            }
         } else {
-            plot <- gguse_facet(plot, ggplot2::facet_null())
             plot$coordinates <- plot_coord
         }
 
