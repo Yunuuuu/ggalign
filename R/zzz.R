4af42511ca96acb2322e75e6353c1a82b3bdb12e
Yunuuuu <yunyunpp96@outlook.com>
2025-07-21 10:01:03 +0800

    add code note

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,9 @@
     s3_register("ggrastr::rasterise", "CraftBox")
     s3_register("ggrastr::rasterise", "QuadLayout")
     s3_register("ggrastr::rasterise", "ChainLayout")
+
+    # In R >= 4.3.0, S7 methods fall back to base Ops behavior when one of the
+    # arguments is not an S7 object. This ensures compatibility in such cases.
     if (getRversion() >= "4.3.0") {
         registerS3method("+", "ggalign::AlignPatches", alignpatches_add_call)
     }
