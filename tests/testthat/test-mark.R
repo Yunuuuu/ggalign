b5d1f2314ead484e35e876e0672532fbcc63838f
Yunuuuu <yunyunpp96@outlook.com>
2025-08-06 21:04:32 +0800

    add unit test for mark_triangle

--- tests/testthat/test-mark.R
+++ tests/testthat/test-mark.R
@@ -1,11 +1,11 @@
-test_that("`ggmark()` works well", {
+test_that("`ggmark()` horizontal works well", {
     set.seed(123)
     small_mat <- matrix(rnorm(72), nrow = 9)
     rownames(small_mat) <- paste0("row", seq_len(nrow(small_mat)))
     colnames(small_mat) <- paste0("column", seq_len(ncol(small_mat)))
 
     expect_doppelganger(
-        "ggmark, with mark_line()",
+        "ggmark horizontal, with mark_line()",
         ggheatmap(small_mat) +
             theme(axis.text.x = element_text(hjust = 0, angle = -60)) +
             anno_right() +
@@ -14,12 +14,74 @@ test_that("`ggmark()` works well", {
             geom_boxplot(aes(.names, value))
     )
     expect_doppelganger(
-        "ggmark, with mark_tetragon()",
+        "ggmark horizontal, with mark_tetragon()",
         ggheatmap(small_mat) +
             theme(axis.text.x = element_text(hjust = 0, angle = -60)) +
             anno_right() +
             align_kmeans(3L) +
             ggmark(mark_tetragon()) +
+            geom_boxplot(aes(.names, value))
+    )
+    expect_doppelganger(
+        "ggmark horizontal, with mark_triangle() orientation to plot",
+        ggheatmap(small_mat) +
+            theme(axis.text.x = element_text(hjust = 0, angle = -60)) +
+            anno_right() +
+            align_kmeans(3L) +
+            ggmark(mark_triangle(orientation = "plot")) +
+            geom_boxplot(aes(.names, value))
+    )
+    expect_doppelganger(
+        "ggmark horizontal, with mark_triangle() orientation to observation",
+        ggheatmap(small_mat) +
+            theme(axis.text.x = element_text(hjust = 0, angle = -60)) +
+            anno_right() +
+            align_kmeans(3L) +
+            ggmark(mark_triangle(orientation = "observation")) +
+            geom_boxplot(aes(.names, value))
+    )
+})
+
+test_that("`ggmark()` vertical works well", {
+    set.seed(123)
+    small_mat <- matrix(rnorm(72), nrow = 9)
+    rownames(small_mat) <- paste0("row", seq_len(nrow(small_mat)))
+    colnames(small_mat) <- paste0("column", seq_len(ncol(small_mat)))
+
+    expect_doppelganger(
+        "ggmark vertical, with mark_line()",
+        ggheatmap(small_mat) +
+            theme(axis.text.x = element_text(hjust = 0, angle = -60)) +
+            anno_top() +
+            align_kmeans(3L) +
+            ggmark(mark_line()) +
+            geom_boxplot(aes(.names, value))
+    )
+    expect_doppelganger(
+        "ggmark vertical, with mark_tetragon()",
+        ggheatmap(small_mat) +
+            theme(axis.text.x = element_text(hjust = 0, angle = -60)) +
+            anno_top() +
+            align_kmeans(3L) +
+            ggmark(mark_tetragon()) +
+            geom_boxplot(aes(.names, value))
+    )
+    expect_doppelganger(
+        "ggmark vertical, with mark_triangle() orientation to plot",
+        ggheatmap(small_mat) +
+            theme(axis.text.x = element_text(hjust = 0, angle = -60)) +
+            anno_top() +
+            align_kmeans(3L) +
+            ggmark(mark_triangle(orientation = "plot")) +
+            geom_boxplot(aes(.names, value))
+    )
+    expect_doppelganger(
+        "ggmark vertical, with mark_triangle() orientation to observation",
+        ggheatmap(small_mat) +
+            theme(axis.text.x = element_text(hjust = 0, angle = -60)) +
+            anno_top() +
+            align_kmeans(3L) +
+            ggmark(mark_triangle(orientation = "observation")) +
             geom_boxplot(aes(.names, value))
     )
 })
