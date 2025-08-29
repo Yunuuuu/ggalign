8c9206850c64ed060f045cdf8d6701bd40a23a91
Yunuuuu <yunyunpp96@outlook.com>
2025-08-29 18:42:27 +0800

    chore: remove tryCatch usage

--- tests/testthat/helper-setup.R
+++ tests/testthat/helper-setup.R
@@ -3,16 +3,24 @@
 #     version of R)
 #   - CI is not set (on CRAN)
 
-expect_doppelganger <- tryCatch(
-    vdiffr::expect_doppelganger,
-    error = function(cnd) {
-        # If vdiffr is not available and visual tests are not explicitly
-        # disabled, raise error.
-        if (!identical(Sys.getenv("VDIFFR_RUN_TESTS"), "false")) {
-            cli_abort("{.pkg vdiffr} is not installed")
-        }
-
-        # Otherwise, assign a dummy function
-        function(...) testthat::skip("vdiffr is not installed.")
+if (is_installed("vdiffr")) {
+    # write_svg <- function(plot, file, title = "") {
+    #     svglite::svglite(file)
+    #     on.exit(grDevices::dev.off())
+    #     # warning: `print_plot()` is not exported by {vdiffr}
+    #     vdiffr:::print_plot(plot, title)
+    # }
+    # function(...) {
+    #     vdiffr::expect_doppelganger(..., writer = write_svg)
+    # }
+    expect_doppelganger <- vdiffr::expect_doppelganger
+} else if (!identical(Sys.getenv("VDIFFR_RUN_TESTS"), "false")) {
+    # If vdiffr is not available and visual tests are not explicitly
+    # disabled, raise error.
+    cli_abort("{.pkg vdiffr} is not installed")
+} else {
+    # Otherwise, assign a dummy function
+    expect_doppelganger <- function(...) {
+        testthat::skip("vdiffr is not installed.")
     }
-)
+}
