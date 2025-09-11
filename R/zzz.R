cc85507ed984c16ac80fed034c71bf049b039748
Yunuuuu <yunyunpp96@outlook.com>
2025-09-11 21:35:53 +0800

    feat: add citation message on package attach

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,20 @@
+.onAttach <- function(libname, pkgname) {
+    version <- utils::packageDescription(pkgname, fields = "Version")
+    packageStartupMessage(paste(
+        "========================================",
+        paste(pkgname, "version", version),
+        "",
+        "If you use it in published research, please cite: ",
+        paste0(
+            "Peng, Y.; Jiang, S.; Song, Y.; et al. ",
+            "ggalign: Bridging the Grammar of Graphics and Biological Multilayered Complexity. ",
+            "Advanced Science. 2025. doi:10.1002/advs.202507799"
+        ),
+        "========================================",
+        sep = "\n"
+    ))
+}
+
 #' @importFrom S7 methods_register
 .onLoad <- function(libname, pkgname) {
     # register ggplot2 theme elements
